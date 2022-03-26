fbe6c1634b0766b74af73d3b59eba7649b51718c
Micha Davis <m.leigh.davis@gmail.com>
2022-03-26 14:07:37 -0700

    completed linked list challenge

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,30 @@
 'use strict';
 
+const { it } = require('eslint/lib/rule-tester/rule-tester');
 // Require our linked list implementation
-const LinkedLilst = require('../index');
+const LinkedList = require('../index');
 
 describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+  let linkedList = new LinkedList();
+
+  it('should create an empty linked list', () => {
+    expect(linkedList.head).toEqual(null);
+  });
+
+  it('should insert a value into the list at the head', () => {
+    linkedList.insert('C');
+    expect(linkedList.head.value).toEqual('C');
+  });
+
+  it('should insert multiple values and return true or false if a value appears in the list', () => {
+    linkedList.insert('B');
+    linkedList.insert('A');
+
+    expect(linkedList.includes('B')).toEqual(true);
+    expect(linkedList.includes('D')).toEqual(false);
+  });
+
+  it('should return a collection of all the values in the list', () => {
+    expect(linkedList.toString()).toEqual('[A] -> [B] -> [C] -> NULL');
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,6 +1,46 @@
 'use strict';
 
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+    let newNode = new Node(value);
+
+    newNode.next = this.head;
+    this.head = newNode;
+  }
+
+  includes(value) {
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        return true;
+      } else {
+        currentNode = currentNode.next;
+      }
+    }
+    return false;
+  }
+
+  toString() {
+    let currentNode = this.head;
+    let result = '';
+    while (currentNode) {
+      result += `[${currentNode.value}] -> `;
+      currentNode = currentNode.next;
+    }
+    result += 'NULL';
+    return result;
+  }
 
 }
 
