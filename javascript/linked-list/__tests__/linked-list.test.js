916498a433f2db78be74e6d51ce9ea638e8c5379
Micha Davis <m.leigh.davis@gmail.com>
2022-03-28 14:42:22 -0700

    completed challenge

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -4,7 +4,7 @@ const { it } = require('eslint/lib/rule-tester/rule-tester');
 // Require our linked list implementation
 const LinkedList = require('../index');
 
-describe('Linked List', () => {
+describe('Linked List Tests', () => {
   let linkedList = new LinkedList();
 
   it('should create an empty linked list', () => {
@@ -26,5 +26,32 @@ describe('Linked List', () => {
 
   it('should return a collection of all the values in the list', () => {
     expect(linkedList.toString()).toEqual('[A] -> [B] -> [C] -> NULL');
+  });
+
+  it('should append a value to the end of the list', () => {
+    linkedList.append('D');
+    let currentNode = linkedList.head;
+    while (currentNode.next) {
+      currentNode = currentNode.next;
+    }
+    expect(linkedList.toString()).toEqual('[A] -> [B] -> [C] -> [D] -> NULL');
+  });
+
+  it('should append multiple values to the end of the list', () => {
+    linkedList.append('F');
+    linkedList.append('G');
+  });
+
+  it('should insert a value before a given node\'s value', () => {
+    linkedList.insertBefore('F', 'E');
+
+    expect(linkedList.toString()).toEqual('[A] -> [B] -> [C] -> [D] -> [E] -> [F] -> [G] -> NULL');
+  });
+
+  it('should insert a value after a given node\'s value', () => {
+    linkedList.insertAfter('G', 'A');
+
+    expect(linkedList.toString())
+      .toEqual('[A] -> [B] -> [C] -> [D] -> [E] -> [F] -> [G] -> [A] -> NULL');
   });
 });
