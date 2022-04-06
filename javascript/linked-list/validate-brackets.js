8f3fc6161314a2a883d2284923646aa2c61212a9
Micha Davis <m.leigh.davis@gmail.com>
2022-04-06 14:50:09 -0700

    wrote and tested validate brackets challenge

---
+++ javascript/linked-list/validate-brackets.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const { Stack } = require('.');
+
+function validateBrackets(string) {
+  let brackets = ['[', '(', '{', ']', ')', '}'];
+  let stack = new Stack();
+
+  for (let char of string) {
+    if (brackets.includes(char)) {
+
+      if (stack.isEmpty()) {
+        stack.push(char);
+      } else {
+
+        if (
+          char === ']' && stack.peek() === '[' ||
+          char === ')' && stack.peek() === '(' ||
+          char === '}' && stack.peek() === '{'
+        ) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+
+      }
+
+    }
+  }
+  console.log(stack);
+  return stack.isEmpty();
+}
+
+module.exports = validateBrackets;
